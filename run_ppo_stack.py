071788cd29a1aa93f7acb00055eb016e68e2e156
Ctiely <814868906@qq.com>
2019-11-01 15:16:00 +0800

    modify

--- run_ppo_stack.py
+++ run_ppo_stack.py
@@ -17,13 +17,13 @@ if __name__ == "__main__":
     
     logging.basicConfig(level=logging.INFO, format='%(asctime)s|%(levelname)s|%(message)s')
 
-    explore_steps = 2048
-    total_updates = 2000
-    save_model_freq = 100
-    action_space = 16
+    explore_steps = 1024
+    total_updates = 1000
+    save_model_freq = 30
+    action_space = 4
     frame_stack = 4
     size = 84
-    death_frame = 30
+    death_frame = 29
     state_space = (size, size, frame_stack)
     
     def obs_fn():
@@ -44,7 +44,7 @@ if __name__ == "__main__":
                 x, 1, kernel_initializer=tf.truncated_normal_initializer()))
         return logit_action_probability, state_value
     
-    ppo = PPO(action_space, obs_fn, model_fn, train_epoch=5, batch_size=32, save_path='./ppo_stack_log')
+    ppo = PPO(action_space, obs_fn, model_fn, train_epoch=5, batch_size=64, save_path='./ppo_log_oneplayer_stack')
     
     env = Raiden2(6666, num_envs=8, with_stack=True)
     env_ids, states, rewards, dones = env.start()
@@ -54,15 +54,15 @@ if __name__ == "__main__":
         nth_trajectory += 1
         for _ in tqdm(range(explore_steps)):
             actions = ppo.get_action(np.asarray(states))
-            actions = [(action // 4, action % 4) for action in actions]
+            actions = [(action, 4) for action in actions]
             env_ids, states, rewards, dones = env.step(env_ids, actions)
 
         s_batchs, a_batchs, r_batchs, d_batchs = env.get_episodes()
-        a_batchs = [[a[0] * 4 + a[1] for a in a_batch] for a_batch in a_batchs]
-        r_batchs = [[sum(r) for r in r_batch] for r_batch in r_batchs]
+        a_batchs = [[a[0] for a in a_batch] for a_batch in a_batchs]
+        r_batchs = [[r[0] for r in r_batch] for r_batch in r_batchs]
         if death_frame > 0:
             batch_size = len(d_batchs)
-            retain_ob_batch = []
+            retain_s_batch = []
             retain_a_batch = []
             retain_r_batch = []
             retain_d_batch = []
@@ -74,11 +74,11 @@ if __name__ == "__main__":
                         r_episode[-death_frame] = r_episode[-1]
                         d_episode[-death_frame] = True
                     
-                    retain_ob_batch.append(s_batchs[i][:-death_frame + 1])
+                    retain_s_batch.append(s_batchs[i][:-death_frame + 1])
                     retain_a_batch.append(a_batchs[i][:-death_frame + 1])
                     retain_r_batch.append(r_batchs[i][:-death_frame + 1])
                     retain_d_batch.append(d_batchs[i][:-death_frame + 1])
-            s_batchs = retain_ob_batch
+            s_batchs = retain_s_batch
             a_batchs = retain_a_batch
             r_batchs = retain_r_batch
             d_batchs = retain_d_batch
@@ -86,7 +86,7 @@ if __name__ == "__main__":
                 nth_trajectory -= 1
                 logging.info("no enough data, continue collect data.")
                 continue
-        
+
         logging.info(
             f'>>>>{env.mean_reward}, nth_trajectory{nth_trajectory}')
         
